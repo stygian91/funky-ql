66e4dfbd5ea88991d85dffb3f9b82e81c38dfedc
stygian91 <kioravuser@gmail.com>
2022-04-02 09:34:17 +0300

    Limit function calls to a whitelist of funky-lib functions.

--- src/transpilers/function.js
+++ src/transpilers/function.js
@@ -1,9 +1,89 @@
+import * as F from 'funky-lib';
+
 import { expression } from "./where";
+
+const permittedFunctionNames = [
+  // math
+  'clamp',
+  'inRange',
+  'modulo',
+  // string
+  'indexOf',
+  'replace',
+  'split',
+  'toLower',
+  'toUpper',
+  'trim',
+  // logic
+  'defaultTo',
+  'equals',
+  'isEmpty',
+  'isNil',
+  'ifElse',
+  'when',
+  'unless',
+  'not',
+  'and',
+  'or',
+  'xor',
+  // function
+  'F',
+  'T',
+  'always',
+  'compose',
+  'identity',
+  'nthArg',
+  'pipe',
+  'tap',
+  // list
+  'all',
+  'any',
+  'difference',
+  'differenceWith',
+  'find',
+  'findIndex',
+  'findLast',
+  'findLastIndex',
+  'flatten',
+  'head',
+  'includes',
+  'intersection',
+  'intersectionWith',
+  'join',
+  'nth',
+  'of',
+  'pluck',
+  'range',
+  'size',
+  'slice',
+  'tail',
+  'take',
+  'takeLast',
+  'times',
+  'union',
+  'unionWith',
+  'unique',
+  // object
+  'has',
+  'hasPath',
+  'keys',
+  'omit',
+  'path',
+  'pathOr',
+  'pathSatisfies',
+  'prop',
+  'propEq',
+  'propSatisfies',
+  'values',
+];
 
 const functionCall = (ast) => {
   const argumentList = functionArguments(ast.arguments);
+  if (!F.includes(ast.functionName, 0, permittedFunctionNames)) {
+    throw new Error('Unknown function name.');
+  }
 
-  return `${ast.functionName}(${argumentList})`;
+  return `F.${ast.functionName}(${argumentList})`;
 };
 
 const functionArguments = (ast) => {
--- test/transpilers/function.test.js
+++ test/transpilers/function.test.js
@@ -3,23 +3,29 @@ import functionCall from "../../src/transpilers/function";
 
 describe('transpiles functions', () => {
   test('regular call', () => {
-    const ast = language.FunctionCall.tryParse('func(1, 2)');
+    const ast = language.FunctionCall.tryParse('clamp(1, 2, 3)');
     const actual = functionCall(ast);
 
-    expect(actual).toEqual('func(1, 2)');
+    expect(actual).toEqual('F.clamp(1, 2, 3)');
   });
 
   test('nested call', () => {
-    const ast = language.FunctionCall.tryParse('func(1, func2(3))');
+    const ast = language.FunctionCall.tryParse('and(1, or(0, 1))');
     const actual = functionCall(ast);
 
-    expect(actual).toEqual('func(1, func2(3))');
+    expect(actual).toEqual('F.and(1, F.or(0, 1))');
   });
 
   test('call with arithmetic', () => {
-    const ast = language.FunctionCall.tryParse('func(1, (41 + 1))');
+    const ast = language.FunctionCall.tryParse('clamp(1, (41 + 1), 3)');
     const actual = functionCall(ast);
 
-    expect(actual).toEqual('func(1, 42)');
+    expect(actual).toEqual('F.clamp(1, 42, 3)');
+  });
+
+  test('throws with unknown functions', () => {
+    const ast = language.FunctionCall.tryParse('qwerty(1, 2, 3)');
+
+    expect(() => functionCall(ast)).toThrow();
   });
 });
